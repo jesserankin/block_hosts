eb08b6be138a9c4bcabd3462a45128da320fbb4f
Jesse Rankin <jesserankin@gmail.com>
2015-08-27 12:57:34 -0400

    refactored and added timed disable of the  block
    for specified # of minutes and tracking how long used per day

--- block.rb
+++ block.rb
@@ -5,6 +5,45 @@ domains = ARGV[1..-1]
 
 lines = []
 
+TIME_PREFIX = 'COUNTER'
+
+def disable(lines)
+  lines.map!{|l| !l.start_with?('#') && l.include?('#BLOCK') ? '#' + l : l }
+end
+
+def enable(lines)
+  lines.map!{|l| l.start_with?('#') && l.include?('#BLOCK') ? l[1..-1] : l }
+end
+
+def date
+  Time.now.strftime('%Y-%m-%d')
+end
+
+def counter_value(lines)
+  counter_line = lines.find{|l| l.include?("##{TIME_PREFIX} #{date}")}
+  counter_line ? counter_line.split(" ")[2].to_i : 0
+end
+
+def delete_counter(lines)
+  lines.delete_if{|l| l.include?("##{TIME_PREFIX} #{date}")}
+end
+
+def set_counter(lines, mins)
+  lines << "##{TIME_PREFIX} #{date} #{mins}\n"
+end
+
+def add_to_counter(lines, mins)
+  total = counter_value(lines) + mins
+  delete_counter(lines)
+  set_counter(lines, total)
+end
+
+def save_file(lines)
+  File.open('/etc/hosts', 'w') do |file|
+    file.write(lines.join)
+  end
+end
+
 File.open('/etc/hosts', 'r') do |file|
   lines = file.readlines
   case command
@@ -19,14 +58,24 @@ File.open('/etc/hosts', 'r') do |file|
       lines.delete_if{|l| l.include?("#BLOCK #{domain}")}
     end
   when 'disable'
-    lines.map!{|l| !l.start_with?('#') && l.include?('#BLOCK') ? '#' + l : l }
+    disable(lines)
   when 'enable'
-    lines.map!{|l| l.start_with?('#') && l.include?('#BLOCK') ? l[1..-1] : l }
+    enable(lines)
+  when 'disable-timer'
+    count = counter_value(lines)
+    disable(lines)
+    save_file(lines)
+    duration = ARGV[1].to_i
+    duration.times do |i|
+      print "\r#{count + i} minutes total today, #{duration - i} minutes left  "
+      sleep 60
+    end
+    print "\rTime's up! #{count + duration} minutes total today                      \n"
+    enable(lines)
+    add_to_counter(lines, duration)
   else
     puts 'Invalid command'
   end
 end
 
-File.open('/etc/hosts', 'w') do |file|
-  file.write(lines.join)
-end
+save_file(lines)
